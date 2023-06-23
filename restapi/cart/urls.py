bff628c7d93a9a89de66c902c5bac58d1bdfdfe6
Sherif <sherif.ma465@gmail.com>
2023-06-23 18:40:17 +0300

    Class views for cart

--- restapi/cart/urls.py
+++ restapi/cart/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
-from . import views
+from .views import AddToCartAPIView, GetCartAPIView
 
 urlpatterns = [
-    # path('', views.index, name="index"),
-    
-    # ________________________________________ Django Cart Task ____________________________
-    path('api/add_to_cart/<int:user_id>', views.add_to_cart_api, name="add_to_cart_api"),
-    path('api/get_cart/<int:user_id>', views.get_cart_api, name="get_cart_api"),
-    path('api/get_cart/<int:user_id>/<int:product_id>', views.get_cart_api, name='delete-cart-product-api'),
-    
+    path('add-to-cart-api/<int:user_id>/', AddToCartAPIView.as_view(), name='add_to_cart_api'),
+    path('get-cart-api/<int:user_id>/', GetCartAPIView.as_view(), name='get_cart_api'),
+    path('get-cart-api/<int:user_id>/<int:product_id>/', GetCartAPIView.as_view(), name='get_cart_api'),
 ]
--- restapi/cart/views.py
+++ restapi/cart/views.py
@@ -1,72 +1,66 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect
+from django.core.mail import send_mail
 from django.http import HttpResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import  permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 
 # ------------------ Django Task ------------------
 from .models import Cart
 from user.models import User
 from product.models import Product
-from .serializers import  CartSerializer
-from django.core.mail import send_mail
-# from rest_framework.permissions import IsAuthenticated
-# @permission_classes([IsAuthenticated])
+from .serializers import CartSerializer
 
 # Create your views here.
-@api_view(['POST'])
-def add_to_cart_api(request, user_id):
-    data = {}
-    try:
-        user = get_object_or_404(User, id=user_id)
-        product_id = request.data.get('product') # to get the product id from the request
-        quantity = request.data.get('quantity') # to get the quantity from the request 
+class AddToCartAPIView(APIView):
+    @permission_classes([IsAuthenticated])
+    def post(self, request, user_id):
+        data = {}
+        try:
+            user = get_object_or_404(User, id=user_id)
+            product_id = request.data.get('product') # to get the product id from the request
+            quantity = request.data.get('quantity') # to get the quantity from the request 
 
-        # if request.user != user:  # Compare the authenticated user with the user in the request
-        #     return Response({'error': 'You are not authorized to perform this action.'}, status=status.HTTP_403_FORBIDDEN)
+            if product_id is None or quantity is None:
+                return Response({'error': 'Product ID and quantity are required.'}, status=status.HTTP_400_BAD_REQUEST)
 
-        if product_id is None or quantity is None:
-            return Response({'error': 'Product ID and quantity are required.'}, status=status.HTTP_400_BAD_REQUEST)
+            product = get_object_or_404(Product, id=product_id)
+            cart_item, created = Cart.objects.get_or_create(user=user, product=product)
+            if int(quantity) <= 0:
+                return Response({'error': 'Quantity must be a positive number.'}, status=status.HTTP_400_BAD_REQUEST)
 
-        product = get_object_or_404(Product, id=product_id)
-        cart_item, created = Cart.objects.get_or_create(user=user, product=product)
-        if int(quantity) <= 0:
-            return Response({'error': 'Quantity must be a positive number.'}, status=status.HTTP_400_BAD_REQUEST)
-
-        
-        if cart_item.quantity + int(quantity) > product.instock:
+            if cart_item.quantity + int(quantity) > product.instock:
                 return Response({'error': 'Requested quantity exceeds available quantity.'}, status=status.HTTP_400_BAD_REQUEST)
-            
-        # Update the quantity if the cart item already exists
-        if not created:
-           
-            cart_item.quantity += int(quantity)
-            
-            cart_item.save()
 
-        cart_serializer = CartSerializer(cart_item)
-        data = cart_serializer.data
-        http_status = status.HTTP_201_CREATED
+            # Update the quantity if the cart item already exists
+            if not created:
+                cart_item.quantity += int(quantity)
+                cart_item.save()
 
-    except User.DoesNotExist:
-        return Response({'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
-    except Product.DoesNotExist:
-        return Response({'error': 'Product not found.'}, status=status.HTTP_404_NOT_FOUND)
-    except Exception as e:
-        print(f'exception in add_to_cart_api => {e}')
-        data = {'error': 'Error occurred while adding item to cart.'}
-        http_status = status.HTTP_500_INTERNAL_SERVER_ERROR
+            cart_serializer = CartSerializer(cart_item)
+            data = cart_serializer.data
+            http_status = status.HTTP_201_CREATED
 
-    return Response(data=data, status=http_status)
+        except User.DoesNotExist:
+            return Response({'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+        except Product.DoesNotExist:
+            return Response({'error': 'Product not found.'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            print(f'exception in add_to_cart_api => {e}')
+            data = {'error': 'Error occurred while adding item to cart.'}
+            http_status = status.HTTP_500_INTERNAL_SERVER_ERROR
+
+        return Response(data=data, status=http_status)
 
 
-@api_view(['GET' , 'PUT' , 'DELETE'])
-def get_cart_api(request,user_id,product_id=None):
-    
-    data = {}
-    try:
-        user = get_object_or_404(User, id=user_id)
-        if request.method == 'GET':
+class GetCartAPIView(APIView):
+    @permission_classes([IsAuthenticated])
+    def get(self, request, user_id, product_id=None):
+        data = {}
+        try:
+            user = get_object_or_404(User, id=user_id)
             if product_id is not None:
                 cart_item = get_object_or_404(Cart, user=user, product_id=product_id)
                 cart_serializer = CartSerializer(cart_item)
@@ -77,8 +71,17 @@ def get_cart_api(request,user_id,product_id=None):
                 cart_serializer = CartSerializer(cart_items, many=True)
                 data = cart_serializer.data
                 http_status = status.HTTP_200_OK
-            
-        if request.method == 'PUT':
+
+        except Exception as e:
+            print(f'exception in get_cart_api => {e}')
+            http_status = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return Response(data=data, status=http_status)
+
+    @permission_classes([IsAuthenticated])
+    def put(self, request, user_id, product_id=None):
+        data = {}
+        try:
+            user = get_object_or_404(User, id=user_id)
             product = get_object_or_404(Product, id=product_id)
             cart_item = get_object_or_404(Cart, user=user, product=product)
             cart_serializer = CartSerializer(instance=cart_item, data=request.data, partial=True)
@@ -88,8 +91,17 @@ def get_cart_api(request,user_id,product_id=None):
                 http_status = status.HTTP_200_OK
             else:
                 return Response(cart_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
-        if request.method == 'DELETE':
+
+        except Exception as e:
+            print(f'exception in get_cart_api => {e}')
+            http_status = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return Response(data=data, status=http_status)
+
+    @permission_classes([IsAuthenticated])
+    def delete(self, request, user_id, product_id=None):
+        data = {}
+        try:
+            user = get_object_or_404(User, id=user_id)
             if product_id is not None:
                 product = get_object_or_404(Product, id=product_id)
                 cart_item = get_object_or_404(Cart, user=user, product=product)
@@ -100,11 +112,11 @@ def get_cart_api(request,user_id,product_id=None):
                 cart_items.delete()
                 send_cart_deleted_email(user)
             http_status = status.HTTP_204_NO_CONTENT
-                
-    except Exception as e:
-        print(f'exception in get_cart_api => {e}')
-        http_status = status.HTTP_500_INTERNAL_SERVER_ERROR
-    return Response(data=data, status=http_status)
+
+        except Exception as e:
+            print(f'exception in get_cart_api => {e}')
+            http_status = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return Response(data=data, status=http_status)
 
 
 def send_cart_deleted_email(user):
